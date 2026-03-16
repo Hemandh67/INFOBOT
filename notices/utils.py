39c3deb72a0b13bada8df908cb63a4cc70bb5cbf
Hemandh-M-S <hemandhhemandh1@gmail.com>
2026-03-16 22:59:03 +0530

    Fix MongoDB SSL Handshake error by adding certifi

--- notices/utils.py
+++ notices/utils.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from django.conf import settings
+import certifi
 
 def get_db_handle():
-    client = MongoClient(settings.MONGO_URI)
+    client = MongoClient(settings.MONGO_URI, tlsCAFile=certifi.where())
     db = client[settings.MONGO_DB_NAME]
     return db, client
